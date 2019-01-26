98c24f318c2bacaeda34d16b923a16be27cb0de7
gulugulu321 <1904392250@qq.com>
2019-01-26 22:17:17 +0800

    format loginservice

--- lender/src/main/java/com/lender/service/LoginService.java
+++ lender/src/main/java/com/lender/service/LoginService.java
@@ -1,6 +1,5 @@
 package com.lender.service;
 
-public interface LoginService
-{
-	public boolean login(String userAccount, String userPassword);
+public interface LoginService {
+    public boolean login(String userAccount, String userPassword);
 }
